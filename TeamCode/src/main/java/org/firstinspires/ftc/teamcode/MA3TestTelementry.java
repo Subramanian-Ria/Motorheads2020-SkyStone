a5dd110e34879675463c8f07e581d8316454dad0
Gabriel Patel <bubble101012@gmail.com>
2019-10-10 17:21:32 -0500

    new odometry alg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MA3TestTelementry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MA3TestTelementry.java
@@ -11,14 +11,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class MA3TestTelementry extends OpMode {
 
     MA3TestHardware robot = new MA3TestHardware();
-    private double rotationCount = 0;
-    boolean rotated = false;
     private ElapsedTime runtime = new ElapsedTime();
-    private double lastVoltage = 0;
-    private double lastPosition = 0;
-    private  double currentPosition = 0;
-    private double velocity = 0;
-
+    private final double diameter = 4;
+    private final double circ = diameter * Math.PI;
+    private final double threshold = .2;
 
     @Override
     public void init()
@@ -32,7 +28,35 @@ public class MA3TestTelementry extends OpMode {
     @Override
     public void loop()
     {
-        if(Math.abs(robot.MA3.getVoltage() - lastVoltage) > .2)
+         getRotations(1, circ*3);
+        /*if(index < 4)
+        {
+            voltages[index] = robot.MA3.getVoltage();
+
+            index++;
+        }
+        else
+        {
+            index = 0;
+            voltages[index] = robot.MA3.getVoltage();
+            index++;
+        }
+        voltageChange = Math.abs(currentVoltage - lastVoltage);
+        if(voltageChange >= threshold)
+        {
+            rotations += voltageChange/5;
+            dist = (voltageChange/5)*circ;
+            totalDist += dist;
+        }
+        if(runtime.milliseconds() >= 250)
+        {
+            velocity = totalDist - lastDistV;
+            lastDistV = totalDist;
+            runtime.reset();
+        }
+
+
+        /*if(Math.abs(robot.MA3.getVoltage() - lastVoltage) > .2)
         {
             rotationCount+=Math.abs(robot.MA3.getVoltage()-lastVoltage)/5;
             lastVoltage = robot.MA3.getVoltage();
@@ -60,12 +84,48 @@ public class MA3TestTelementry extends OpMode {
             lastPosition = currentPosition;
             runtime.reset();
         }*/
-        telemetry.addData("MA3", robot.MA3.getVoltage());
-        //telemetry.addData("rotated", rotated);
-        telemetry.addData("Rotations", rotationCount);
-        telemetry.addData("Velocity (in/s)", velocity);
-        telemetry.update();
+//        telemetry.addData("MA3", robot.MA3.getVoltage());
+//        telemetry.addData("Rotations", rotations);
+//        telemetry.addData("Total Distance", totalDist);
+//        telemetry.addData("Velocity (in/s)", velocity);
+        /*telemetry.update();
+        lastVoltage = robot.MA3.getVoltage();*/
     }
+    public void getRotations(int dir, double endDistIn)
+    {
+        double totalDist = 0;
+        double lastVolt = 0;
+        double currentVolt;
+        double changeVolt;
+        telemetry.addData("Total Distance", totalDist);
+        telemetry.addData("MA3", robot.MA3.getVoltage());
+        telemetry.addData("Rotations", totalDist/circ);
+        while(totalDist < endDistIn)
+        {
+            currentVolt = robot.MA3.getVoltage();
+            changeVolt = currentVolt - lastVolt;
+            if(Math.abs(changeVolt) > threshold)
+            {
+                if(dir > 0)
+                {
+                    if(changeVolt < 0)
+                    {
+                        changeVolt += 5;
 
-
+                    }
+                    totalDist += (changeVolt/5)*circ;
+                }
+                else
+                {
+                    if(changeVolt > 0)
+                    {
+                        changeVolt-=5;
+                    }
+                    totalDist+= Math.abs(changeVolt/5)*circ;
+                }
+            }
+            lastVolt = currentVolt;
+            telemetry.update();
+        }
+    }
 }
