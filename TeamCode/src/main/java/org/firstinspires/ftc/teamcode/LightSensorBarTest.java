f1299bb29c8695f0b9b08d111dab2e4d7a5ec0f2
DrakeDemolishr <43097520+DrakeDemolishr@users.noreply.github.com>
2019-10-28 17:18:26 -0500

    lightbar success - check comments for things and read code for important values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LightSensorBarTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@TeleOp(name="LightSensorBarTest", group="ServoTest")
+//@Disabled
+
+public class LightSensorBarTest extends OpMode {
+
+    LightSensorBarTestHardware robot = new LightSensorBarTestHardware();
+
+    private float drive = .8f;
+
+    @Override
+    public void init()
+    {
+        //Initialize the hardware variables.
+        //The init() method of the hardware class does all the work here
+        robot.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop()
+    {
+        //
+        telemetry.addData("sen1", robot.sen1.alpha());
+        telemetry.addData("sen2", robot.sen2.alpha());
+        telemetry.addData("sen3", robot.sen3.alpha());
+        telemetry.addData("dist", robot.dist.getDistance(DistanceUnit.INCH));//if the distance is < .5in then the stone is too close despite being skystone
+        //and if a nonskystone is more than 5.5in away it needs to be brought closer - if stone is really close then skystone reading are lower across the board than regular stone
+        double average = robot.sen1.alpha()+ robot.sen3.alpha() + robot.sen2.alpha();
+        boolean isSkystone = false;
+        average/=3;
+        telemetry.addData("average", (robot.sen1.alpha() + robot.sen2.alpha() + robot.sen3.alpha())/3);
+        if(average < 150)
+        {
+            isSkystone = true;
+        }
+        telemetry.addData("Is A Skystone", isSkystone);
+        telemetry.update();
+        //not funny didnt laugh
+    }
+
+}
