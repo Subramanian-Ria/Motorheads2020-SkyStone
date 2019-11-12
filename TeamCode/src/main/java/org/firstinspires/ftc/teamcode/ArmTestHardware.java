02f8b848ecb4b677cd8b159acb84dafbcc9b7ac8
DrakeDemolishr <43097520+DrakeDemolishr@users.noreply.github.com>
2019-11-12 08:33:42 -0600

    color sensor things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestHardware.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ArmTestHardware
+{
+
+    //public DistanceSensor distSen;
+    public DcMotor armTest;
+   // public Servo servo2;
+
+    //declaring values for use with encoders
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // AndyMark Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 0.19685039;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     VERT_INCHES             = 3.0;      //inches to raise verticalarm       TODO: test for real value
+    static final double     HORZ_INCHES             = 3.0;      //inches to extend horzizantalarm   TODO: test for real value
+    static final double     EN_ARM_SPEED            = 0.2;      //speed of arm movement
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define Motors
+        armTest = hwMap.get(DcMotor.class, "armTest");
+        armTest.setDirection(DcMotorSimple.Direction.FORWARD);
+        //servo2 = hwMap.get(Servo.class, "servo2");
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestTeleop.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="ArmTestTeleop", group="ServoTest")
+//@Disabled
+
+public class ArmTestTeleop extends OpMode {
+
+    ArmTestHardware robot = new ArmTestHardware();
+
+    private float drive = .8f;
+
+    @Override
+    public void init()
+    {
+        //Initialize the hardware variables.
+        //The init() method of the hardware class does all the work here
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop()
+    {
+//        if(gamepad1.b)
+//        {
+//            if(robot.susanTest.getPower() != 0)
+//            {
+//                double inc = -(robot.susanTest.getPower()/200);
+//                for(int i = 0; i < 200; i++)
+//                {
+//                    robot.susanTest.setPower(robot.susanTest.getPower() + inc);
+//                }
+//            }
+//            //robot.susanTest.setPower(0);
+//           // robot.servo2.setPosition(1);
+//        }
+//        if(gamepad1.x)
+//        {
+//            if(robot.susanTest.getPower() != .45)
+//            {
+//                double inc = (.45 - robot.susanTest.getPower())/200;
+//                for(int i = 0; i < 200; i++)
+//                {
+//                    robot.susanTest.setPower(robot.susanTest.getPower() + inc);
+//                }
+//            }
+//            //robot.susanTest.setPower(.45);
+//            //robot.servo2.setPosition(0.5);
+//        }
+//        if(gamepad1.y)
+//        {
+//            if(robot.susanTest.getPower() != -.45)
+//            {
+//                double inc = (-.45 - robot.susanTest.getPower())/200;
+//                for(int i = 0; i < 200; i++)
+//                {
+//                    robot.susanTest.setPower(robot.susanTest.getPower() + inc);
+//                }
+//            }
+//            //robot.susanTest.setPower(-.45);
+//            // robot.servo2.setPosition(1);
+//        }
+        if(gamepad1.a)
+        {
+            robot.armTest.setPower(.55);
+        }
+        else if(gamepad1.b)
+        {
+            robot.armTest.setPower(-.55);
+        }
+        else
+        {
+            robot.armTest.setPower(0);
+        }
+    }
+
+}
