45759d3fee2484c97908e7dd91f5b0b724d03ac6
DrakeDemolishr <43097520+DrakeDemolishr@users.noreply.github.com>
2019-11-13 18:06:21 -0600

    wrote some things. basic red side stone spawn auton. The pathing logic is correct but I am almost certain that the logic for the arm extension can be improved in some way but I very tired and don't want to use brainpower that I am going to have to devote to hw later. Thanks. bye.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneHardware.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SkyStoneHardware
+{
+    public DcMotor fLMotor;
+    public DcMotor fRMotor;
+    public DcMotor bLMotor;
+    public DcMotor bRMotor;
+    public DcMotor susan;
+    public DcMotor armExt;
+    //public DistanceSensor distSen;
+    //public Servo servo1;
+    //public Servo servo2;
+    //public ColorSensor sensorColor;
+
+    //declaring values for use with encoders
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // AndyMark Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 0.19685039;     // For fwiguring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     VERT_INCHES             = 3.0;      //inches to raise verticalarm       TODO: test for real value
+    static final double     HORZ_INCHES             = 3.0;      //inches to extend horzizantalarm   TODO: test for real value
+    static final double     EN_ARM_SPEED            = 0.2;      //speed of arm movement
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define Motors
+        fLMotor = hwMap.get(DcMotor.class, "fLMotor");
+        fRMotor = hwMap.get(DcMotor.class, "fRMotor");
+        bRMotor = hwMap.get(DcMotor.class, "bRMotor");
+        bLMotor = hwMap.get(DcMotor.class, "bLMotor");
+        susan = hwMap.get(DcMotor.class, "susan");
+        armExt = hwMap.get(DcMotor.class, "armExt");
+        //distSen = hwMap.get(DistanceSensor.class, "distSen");
+        //sensorColor = hwMap.get(ColorSensor.class, "sensorColor");
+        //servo1 = hwMap.get(Servo.class, "servo1");
+        //servo2 = hwMap.get(Servo.class, "servo2");
+
+        fLMotor.setPower(0);
+        bLMotor.setPower(0);
+        fRMotor.setPower(0);
+        bRMotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        fLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        fLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //flipped these 4
+        fLMotor.setDirection(DcMotor.Direction.FORWARD);
+        fRMotor.setDirection(DcMotor.Direction.FORWARD);
+        bLMotor.setDirection(DcMotor.Direction.FORWARD);
+        bRMotor.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+}
